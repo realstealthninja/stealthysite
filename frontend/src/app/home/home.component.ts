c5457a0ebe4d99b3a953d214b299ae7e16a603fe
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-12-12 22:51:44 +0530

    feat: add canvas with gl

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,30 +1,91 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Webgl } from '../webgl/webgl';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
+    const canvas: HTMLCanvasElement = <HTMLCanvasElement>(
+      document.querySelector('#header-canvas')!
+    );
 
-    const canvas: HTMLCanvasElement = 
-     <HTMLCanvasElement>  document.querySelector("#header-canvas")!;
-    
-    const gl: WebGLRenderingContext = canvas.getContext("webgl")!;
+    combineLatest([
+      this.httpClient.get('shaders/hello.vert', { responseType: 'text' }),
+      this.httpClient.get('shaders/hello.frag', { responseType: 'text' }),
+    ]).subscribe(([vertex, fragment]) => {
+      var gl = new Webgl(canvas, vertex, fragment);
 
-    if (gl === null) {
-      alert("No webgl");
-      return;
-    }
+      var positionAttributeLocation = gl.context.getAttribLocation(
+        gl.program,
+        "a_position"
+      );
+
+      var positionBuffer = gl.  context.createBuffer();
+      gl.context.bindBuffer(gl.context.ARRAY_BUFFER, positionBuffer);
 
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+      var positions = [
+        -1.0, -1.0,
+        -1.0,  1.0,
+         1.0,  1.0,
+      ];
 
-    gl.clear(gl.COLOR_BUFFER_BIT);
+      gl.context.bufferData(
+        gl.context.ARRAY_BUFFER,
+        new Float32Array(positions),
+        gl.context.STATIC_DRAW
+      );
+
+      var vao = gl.context.createVertexArray();
+      
+      gl.context.bindVertexArray(vao);
+      gl.context.enableVertexAttribArray(positionAttributeLocation);
+
+      var size = 2; // no of elements per row
+      var type = gl.context.FLOAT;
+      var normalize = false;
+      var stride = 0;
+      var offset = 0;
+
+      gl.context.vertexAttribPointer(
+        positionAttributeLocation,
+        size,
+        type,
+        normalize,
+        stride,
+        offset
+      );
+
+      
+
+      gl.context.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+      var iresloc = gl.context.getUniformLocation(gl.program, "iResolution");
+      gl.context.uniform3f(iresloc, gl.canvas.width, gl.canvas.height, 1.0);
+
+      //clear canvas
+      gl.context.clearColor(0.0, 0.0, 0.0, 1.0);
+
+      gl.context.clear(gl.context.COLOR_BUFFER_BIT);
+      gl.context.useProgram(gl.program);
+
+      gl.context.bindVertexArray(vao);
+      var primitiveType = gl.context.TRIANGLES;
+      var offset = 0;
+      var count = 3;
+      gl.context.drawArrays(primitiveType, offset, count);
+
+
+    });
+
   }
 }
