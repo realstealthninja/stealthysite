fe70e06cd2bd4b8758c065165d6c42b9df5defdd
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-12-23 11:17:49 +0530

    feat: add glsl canvas to home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,18 +1,19 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Webgl } from '../webgl/webgl';
-import { combineLatest } from 'rxjs';
 import { VERSION } from '@angular/core';
 import {version} from '../../../package.json';
+import { GlslComponent } from "../glsl/glsl.component";
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
+  imports: [GlslComponent],
 })
 export class HomeComponent {
   angular_version = VERSION.full;
   project_version = version;
+  ngOnInit() {
+  }
 }
