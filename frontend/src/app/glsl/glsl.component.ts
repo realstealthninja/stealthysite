26c1117be394c314cad7889b8c4c6bf662a1a332
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-02-07 08:45:50 +0530

    fix: scaling in glsl is fixed.

--- frontend/src/app/glsl/glsl.component.ts
+++ frontend/src/app/glsl/glsl.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { combineLatest } from 'rxjs';
 import { Webgl } from '../webgl/webgl';
 import { HttpClient } from '@angular/common/http';
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './glsl.component.html',
   styleUrl: './glsl.component.css',
 })
-export class GlslComponent {
+export class GlslComponent implements OnInit {
   private startTime = 0;
   private canvas: HTMLCanvasElement | null = null;
 
@@ -18,14 +18,41 @@ export class GlslComponent {
   }
 
   ngOnInit() {
+    function setUniform(
+    gl: Webgl,
+    name: string,
+   data: number[]
+    ) {
+    const loc = gl.context.getUniformLocation(
+      gl.program, name
+    );
+
+    switch (data.length) {
+      case 1:
+
+        gl.context.uniform1f(loc, data[0]);
+        break;
+      case 2:
+        gl.context.uniform2f(loc, data[0], data[1]);
+        break;
+      case 3:
+        gl.context.uniform3f(loc, data[0], data[1], data[2]);
+        break;
+      default:
+
+    }
+
+
+    }
+
     function hexToRgb(hex: string) {
       // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+      const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
       hex = hex.replace(shorthandRegex, function (m, r, g, b) {
         return r + r + g + g + b + b;
       });
 
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
       return result
         ? {
             r: parseInt(result[1], 16),
@@ -38,44 +65,65 @@ export class GlslComponent {
       // Lookup the size the browser is displaying the canvas in CSS pixels.
       const displayWidth  = canvas.clientWidth;
       const displayHeight = canvas.clientHeight;
-     
+
       // Check if the canvas is not the same size.
       const needResize = canvas.width  !== displayWidth ||
                          canvas.height !== displayHeight;
-     
+
       if (needResize) {
         // Make the canvas the same size
         canvas.width  = displayWidth;
         canvas.height = displayHeight;
+
+        console.log("scaling");
       }
-     
+
       return needResize;
     }
 
-    this.canvas = document.getElementById('gl-canvas') as HTMLCanvasElement;
-    combineLatest([
-      this.httpClient.get('shaders/hello.vert', { responseType: 'text' }),
-      this.httpClient.get('shaders/hello.frag', { responseType: 'text' }),
-    ]).subscribe(([vertex, fragment]) => {
-      var gl = new Webgl(this.canvas!, vertex, fragment);
+    this.canvas = document.getElementById(
+      'gl-canvas'
+    ) as HTMLCanvasElement;
 
-      var positionAttributeLocation = gl.context.getAttribLocation(
+    combineLatest(
+      [
+        this.httpClient.get(
+          'shaders/hello.vert',
+          { responseType: 'text' }
+        ),
+
+        this.httpClient.get(
+          'shaders/hello.frag',
+          { responseType: 'text' }
+        ),
+      ]
+    ).subscribe(([vertex, fragment]) => {
+
+      const gl = new Webgl(this.canvas!, vertex, fragment);
+
+      const positionAttributeLocation =
+        gl.context.getAttribLocation(
         gl.program,
         'a_position'
       );
 
-      var positionBuffer = gl.context.createBuffer();
-      gl.context.bindBuffer(gl.context.ARRAY_BUFFER, positionBuffer);
+      const positionBuffer = gl.context.createBuffer();
 
-      var positions = [
+      gl.context.bindBuffer(
+        gl.context.ARRAY_BUFFER,
+        positionBuffer
+      );
+
+      const positions = [
         -1.0, -1.0,
-        -1.0, 1.0,
-        1.0, 1.0,
-        1.0, -1.0]
+        -1.0,  1.0,
+         1.0,  1.0,
+         1.0, -1.0
+      ]
 
-      var indices = [0, 1, 2, 0, 2, 3];
+      const indices = [0, 1, 2, 0, 2, 3];
 
-      var vao = gl.context.createVertexArray();
+      const vao = gl.context.createVertexArray();
       gl.context.bindVertexArray(vao);
       gl.context.bufferData(
         gl.context.ARRAY_BUFFER,
@@ -83,21 +131,26 @@ export class GlslComponent {
         gl.context.STATIC_DRAW
       );
 
-      var ebo = gl.context.createBuffer();
-      gl.context.bindBuffer(gl.context.ELEMENT_ARRAY_BUFFER, ebo);
+      const ebo = gl.context.createBuffer();
+
+      gl.context.bindBuffer(
+        gl.context.ELEMENT_ARRAY_BUFFER, ebo
+      );
+
       gl.context.bufferData(
         gl.context.ELEMENT_ARRAY_BUFFER,
         new Uint16Array(indices),
         gl.context.STATIC_DRAW
       );
 
-      gl.context.enableVertexAttribArray(positionAttributeLocation);
+      gl.context.enableVertexAttribArray(
+        positionAttributeLocation
+      );
 
-      var size = 2; // no of elements per row
-      var type = gl.context.FLOAT;
-      var normalize = false;
-      var stride = 0;
-      var offset = 0;
+      const size = 2; // no of elements per row
+      const type = gl.context.FLOAT; const normalize = false;
+      const stride = 0;
+      const offset = 0;
 
       gl.context.vertexAttribPointer(
         positionAttributeLocation,
@@ -109,45 +162,67 @@ export class GlslComponent {
       );
 
 
-      var iresloc = gl.context.getUniformLocation(gl.program, 'iResolution');
-      var iTimeloc = gl.context.getUniformLocation(gl.program, 'iTime');
+      const iresloc = gl.context.getUniformLocation(
+        gl.program, 'iResolution'
+      );
+      const iTimeloc = gl.context.getUniformLocation(
+        gl.program, 'iTime'
+      );
 
-      var background = gl.context.getUniformLocation(gl.program, 'background');
-      var accent = gl.context.getUniformLocation(gl.program, 'accent');
- 
-      var secondary = gl.context.getUniformLocation(gl.program, "secondary");
-      var dark = gl.context.getUniformLocation(gl.program, "dark");
+      const background = gl.context.getUniformLocation(
+        gl.program, 'background'
+      );
+      const accent = gl.context.getUniformLocation(
+        gl.program, 'accent'
+      );
 
-      let startTime = this.startTime;
+      const secondary = gl.context.getUniformLocation(
+        gl.program, "secondary"
+      );
+
+      const dark = gl.context.getUniformLocation(
+        gl.program, "dark"
+      );
+
+      const startTime = this.startTime;
       const rgb_norm = 1 / 255;
 
-      gl.context.viewport(0, 0, gl.canvas.clientWidth, gl.canvas.clientHeight);
+      
+
       function render() {
+
         resizeCanvasToDisplaySize(gl.canvas);
+        gl.context.viewport(
+          0, 0,
+          gl.canvas.width,
+          gl.canvas.height
+        );
         //clear canvas
         gl.context.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.context.clear(gl.context.COLOR_BUFFER_BIT);
 
-        var color = window
+        const color = window
           .getComputedStyle(document.body)
           .getPropertyValue('--background');
-        
-        var accent_color = window
+
+        const accent_color = window
           .getComputedStyle(document.body)
           .getPropertyValue('--accent');
 
-        var secondary_hex = window
+        const secondary_hex = window
         .getComputedStyle(document.body)
         .getPropertyValue('--secondary');
-        
-        var theme = window.matchMedia("(prefers-color-scheme: dark)");
 
-        var rgb = hexToRgb(color)!;
-        var secondary_rgb = hexToRgb(secondary_hex)!;
-        var rgb_normalised = [rgb.r / 255, rgb.g / 255, rgb.b / 255];
+        const theme = window.matchMedia(
+          "(prefers-color-scheme: dark)"
+        );
 
-        var accent_rgb = hexToRgb(accent_color)!;
-        var accent_normalised = [
+        const rgb = hexToRgb(color)!;
+        const secondary_rgb = hexToRgb(secondary_hex)!;
+        const rgb_normalised = [rgb.r / 255, rgb.g / 255, rgb.b / 255];
+
+        const accent_rgb = hexToRgb(accent_color)!;
+        const accent_normalised = [
           accent_rgb.r * rgb_norm,
           accent_rgb.g * rgb_norm,
           accent_rgb.b * rgb_norm,
@@ -181,7 +256,7 @@ export class GlslComponent {
 
 
         if(theme.matches) {
-          
+
           gl.context.uniform1i(dark, 1);
         } else {
           gl.context.uniform1i(dark, 0);
