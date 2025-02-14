2a14d2502d3c352e6038952bc7f31303a7472249
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-02-14 13:50:41 +0530

    test: fix tests for shader and webgl

--- frontend/src/app/webgl/shader.spec.ts
+++ frontend/src/app/webgl/shader.spec.ts
@@ -2,6 +2,18 @@ import { Shader } from './shader';
 
 describe('Shader', () => {
   it('should create an instance', () => {
-    expect(new Shader()).toBeTruthy();
+
+    var shaderSource = 
+    `#version 300 es
+
+    in vec4 a_position;
+
+    void main() {
+      gl_Position = a_position;
+    }
+    `
+
+    var context: WebGL2RenderingContext = document.createElement("canvas").getContext("webgl2") as WebGL2RenderingContext;
+    expect(new Shader(context, context.VERTEX_SHADER, shaderSource)).toBeTruthy();
   });
 });
--- frontend/src/app/webgl/webgl.spec.ts
+++ frontend/src/app/webgl/webgl.spec.ts
@@ -2,6 +2,30 @@ import { Webgl } from './webgl';
 
 describe('Webgl', () => {
   it('should create an instance', () => {
-    expect(new Webgl()).toBeTruthy();
+  
+    var canvas: HTMLCanvasElement = document.createElement("canvas") as HTMLCanvasElement;
+    
+    var vert = 
+    `#version 300 es
+
+    in vec4 a_position;
+
+    void main() {
+      gl_Position = a_position;
+    }
+    `;
+
+    var frag = 
+    `#version 300 es
+
+    out vec4 outColor;
+
+    void main() {
+      ouColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+    `
+
+
+    expect(new Webgl(canvas, vert, frag)).toBeTruthy();
   });
 });
