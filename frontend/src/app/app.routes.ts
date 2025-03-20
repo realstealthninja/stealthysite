e372ed0a95449c5bb226391d1968217ba145abf4
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-03-20 21:08:39 +0530

    fix: add blogs to path

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
-    { path: "projects", component: ProjectsComponent}
+    { path: "projects", component: ProjectsComponent},
+    { path: "blogs", component: BlogsComponent}
 ];
