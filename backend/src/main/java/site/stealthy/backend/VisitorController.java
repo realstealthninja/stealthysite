7ae524e438abc9767135ddfda6bb068d7bcf33ea
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-06-01 22:01:34 +0530

    chore: use /api/v1 prefix

--- backend/src/main/java/site/stealthy/backend/VisitorController.java
+++ backend/src/main/java/site/stealthy/backend/VisitorController.java
@@ -4,17 +4,20 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class VisitorController {
     private AtomicLong visitorCount = new AtomicLong(0);
 
-    @PostMapping("/api/visitor/new")
+    @PostMapping("/visitor/new")
     public void new_visitor() {
+        visitorCount.addAndGet(1);
     }
 
-    @GetMapping("/api/visitor/count")
+    @GetMapping("/visitor/count")
     public int get_visitor() {
         return visitorCount.intValue();
     }
