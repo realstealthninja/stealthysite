897930c9e9be3073425b3361bc0edf3b12be6a69
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-06-04 10:26:33 +0530

    fix: add java version

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 import project from '../../../../package.json';
 import { VersionService } from '../../services/version/version.service';
+import { catchError, EMPTY, forkJoin} from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -12,22 +13,33 @@ import { VersionService } from '../../services/version/version.service';
 export class FooterComponent {
   angular_version: string = VERSION.full;
   project_version: string = project.version;
-  spring_version: string | null = null; 
+  spring_version: string | null = null;
+  java_version: string | null = null;
 
   loading: boolean = true;
 
   constructor (private versionService: VersionService) {}
 
   ngOnInit(): void {
-    this.versionService.getSpringVersion().subscribe({
-      next: (version) => {
-        this.spring_version = version;
-        this.loading = false;
-      },
-      error: (err) => {
-        console.log("backend is not online");
+    forkJoin(
+      {
+        java: this.versionService.getJavaVersion(),
+        spring: this.versionService.getSpringVersion()
+      }
+    ).pipe(
+      catchError(
+        err => {
+          console.log(err);
+          this.loading = false;
+          return EMPTY;
+        }
+      )
+    ).subscribe(
+      result => {
+        this.java_version = result.java;
+        this.spring_version = result.spring;
         this.loading = false;
       }
-    })
+    )
   }
 }
