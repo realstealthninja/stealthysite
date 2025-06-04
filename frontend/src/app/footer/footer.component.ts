970d0bbf267a4237fe6523c046fec28b700fc1a6
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-06-04 09:17:35 +0530

    fix: move footer to another component

---
+++ frontend/src/app/footer/footer.component.ts
@@ -0,0 +1,33 @@
+import { Component, VERSION } from '@angular/core';
+import project from '../../../package.json';
+import { VersionService } from '../services/version.service';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  angular_version: string = VERSION.full;
+  project_version: string = project.version;
+  spring_version: string | null = null; 
+
+  loading: boolean = true;
+
+  constructor (private versionService: VersionService) {}
+
+  ngOnInit(): void {
+    this.versionService.getSpringVersion().subscribe({
+      next: (version) => {
+        this.spring_version = version;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.log("backend is not online");
+        this.loading = false;
+      }
+    })
+  }
+}
