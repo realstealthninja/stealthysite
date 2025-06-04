51f6f9112cc5f920c90422806b4dc1197df9b727
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-06-04 22:33:09 +0530

    chore: move version controller into version folder

---
+++ backend/src/main/java/site/stealthy/backend/Version/VersionController.java
@@ -0,0 +1,42 @@
+package site.stealthy.backend.Version;
+
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.SpringVersion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@RestController
+@RequestMapping("/api/v1/version")
+public class VersionController {
+    @Autowired
+    ObjectMapper mapper;
+
+
+    @GetMapping("spring")
+    ObjectNode spring() {
+        ObjectNode versionObject = mapper.createObjectNode();
+        versionObject.put("name", "spring");
+        versionObject.put("version", SpringVersion.getVersion());
+        return versionObject;
+    }
+
+    @GetMapping("java")
+    ObjectNode java() {
+        ObjectNode versionObject = mapper.createObjectNode();
+        versionObject.put("name", "java");
+        versionObject.put("version", Runtime.version().toString());
+        return versionObject;
+    }
+
+    @GetMapping("postgresql")
+    String postgresql() {
+        return "";
+    }
+}
