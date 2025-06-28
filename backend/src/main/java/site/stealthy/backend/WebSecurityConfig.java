1a2a903ce1771c9585b883a2df055e83955c7316
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-06-28 18:36:05 +0530

    fix: implement websecurity config

--- backend/src/main/java/site/stealthy/backend/WebSecurityConfig.java
+++ backend/src/main/java/site/stealthy/backend/WebSecurityConfig.java
@@ -1,18 +1,53 @@
 package site.stealthy.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import site.stealthy.backend.User.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration configuration
+    ) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.build();
- 
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                    auth -> auth
+                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/blog/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/profile/**").hasAnyRole("USER", "ADMIN", "BLOGGER")
+                        .requestMatchers("/blog/create/**").hasAnyRole("BLOGGER", "ADMIN")
+                        .anyRequest().authenticated()
+                    )
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
