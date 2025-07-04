08a068b19f14bacdd7423c74fe99a216108d8b0c
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-07-04 05:50:44 +0530

    fix: remove merge type

--- backend/src/main/java/site/stealthy/backend/User/User.java
+++ backend/src/main/java/site/stealthy/backend/User/User.java
@@ -40,13 +40,14 @@ public class User {
     private boolean approved = false;
     private boolean blogger = false;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
                                     inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private Set<Role> roles;
 
+    public User() {}
 
-    User (
+    public User (
         String username,
         String firstname,
         String lastname,
