0ba883123a03440d26092de96618e8c30e8f292d
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-07-04 05:52:20 +0530

    feat: add registration

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { registerDTO } from '../../interfaces/registerDTO';
+import { UserauthService } from '../../services/userauth/userauth.service';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +16,8 @@ export class RegisterComponent {
 
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private userAuth: UserauthService
   ) {}
 
   ngOnInit() {
@@ -23,12 +26,19 @@ export class RegisterComponent {
       firstName: [''],
       lastName: [''],
       username: ['', Validators.required],
-      password: ['', Validators.required, Validators.minLength(8), Validators.pattern("/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/")],
-      email: ['', Validators.required, Validators.email],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern("/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/")]],
+      email: ['', [Validators.required, Validators.email]],
     })
 
   }
 
+  get firstname() {
+    return this.registrationForm.get('firstname');
+  }
+
+  get lastname() {
+    return this.registrationForm.get('lastname');
+  }
 
   get username() {
     return this.registrationForm.get('username');
@@ -43,6 +53,17 @@ export class RegisterComponent {
   }
 
   onSubmit() {
+    let user: registerDTO = {
+      firstname: this.firstname?.value,
+      lastname: this.lastname?.value,
+      username: this.username?.value,
+      password: this.password?.value,
+      email: this.email?.value
+    };
+
     
+    let hello = this.userAuth.registerUser(user);
+    console.log(hello);
+
   }
 }
