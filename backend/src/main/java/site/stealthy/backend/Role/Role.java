342a6630b07ccf17fb40f9788a4d49a11b2d99f4
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-07-04 05:50:08 +0530

    fix: all roles must be unique

--- backend/src/main/java/site/stealthy/backend/Role/Role.java
+++ backend/src/main/java/site/stealthy/backend/Role/Role.java
@@ -6,9 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "roles", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "name")
+})
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
