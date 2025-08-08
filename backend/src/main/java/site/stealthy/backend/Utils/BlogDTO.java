2065f114b3082bd67101d325ac57df4797d04336
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-08-08 22:09:59 +0530

    feat: create blogDTO

---
+++ backend/src/main/java/site/stealthy/backend/Utils/BlogDTO.java
@@ -0,0 +1,28 @@
+package site.stealthy.backend.Utils;
+
+import java.util.Set;
+
+public class BlogDTO {
+    private String title;
+    
+    private Set<String> tags;
+    private String content;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Set<String> getTags() {
+        return tags;
+    }
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
