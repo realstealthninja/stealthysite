b9c74457f4d991c79d59edc5edb3b9cfbe0f7542
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-08-09 22:42:47 +0530

    chore: use Set instead of list

--- backend/src/main/java/site/stealthy/backend/Blog/BlogRepository.java
+++ backend/src/main/java/site/stealthy/backend/Blog/BlogRepository.java
@@ -1,6 +1,5 @@
 package site.stealthy.backend.Blog;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -12,10 +11,9 @@ import site.stealthy.backend.User.User;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, Long> {
-    Optional<Blog> findBlogById(Long id);
-    Optional<List<Blog>> findBlogsByAuthor(User user);
-    Optional<List<Blog>> findBlogsByTags(Set<String> tags);
+    Optional<Set<Blog>> findBlogsByAuthor(User user);
+    Optional<Set<Blog>> findBlogsByTags(Set<String> tags);
 
     @NativeQuery("SELECT * FROM blogs ORDER BY created_on DESC;")
-    Optional<List<Blog>> getAllBlogs();
+    Optional<Set<Blog>> getAllBlogs();
 }
