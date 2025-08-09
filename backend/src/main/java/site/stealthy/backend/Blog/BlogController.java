88f0ec556cd56992ff175765526c8c36efedf2db
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-08-09 22:44:00 +0530

    fix: use json seralisation to convert classes into json objects

--- backend/src/main/java/site/stealthy/backend/Blog/BlogController.java
+++ backend/src/main/java/site/stealthy/backend/Blog/BlogController.java
@@ -1,11 +1,13 @@
 package site.stealthy.backend.Blog;
 
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import jakarta.websocket.server.PathParam;
 import site.stealthy.backend.User.UserRepository;
 import site.stealthy.backend.Utils.BlogDTO;
 
@@ -33,24 +35,31 @@ public class BlogController {
 
     @GetMapping("/blogs/")
     public ResponseEntity<ObjectNode> listBlogs() {
-        ObjectNode hello = mapper.createObjectNode();
-        hello.put("hi", "hello");
-        return (new ResponseEntity<ObjectNode>(hello, HttpStatus.OK));
+        ObjectNode ret = mapper.createObjectNode();
+        Optional<Set<Blog>> blogs = blogRepository.getAllBlogs();
+        if (blogs.isEmpty()) {
+            ret.putArray("blogs");
+            return (new ResponseEntity<ObjectNode>(ret, HttpStatus.OK));
+        }
+        ArrayNode blogarr = ret.putArray("blogs");
+        
+        for (Blog blog : blogs.get()) {
+            blogarr.add(
+                mapper.convertValue(blog, ObjectNode.class)
+            );
+        }
+
+        return (new ResponseEntity<ObjectNode>(ret, HttpStatus.OK));
     }
 
     @GetMapping("/blogs/{id}")
-    public ResponseEntity<ObjectNode> getBlogbyId(@PathParam("id") Long id) {
+    public ResponseEntity<ObjectNode> getBlogbyId(@PathVariable("id") Long id) {
         ObjectNode respObject = mapper.createObjectNode();
-        Optional<Blog> blog = blogRepository.findBlogById(id);
+        Optional<Blog> blog = blogRepository.findById(id);
         if (blog.isEmpty()) {
             respObject.put("error", "Could not find blog");
         } else {
-            respObject.put("id", blog.get().getId());
-            respObject.put("title", blog.get().getTitle());
-        
-            respObject.put("content", blog.get().getContent());
-            respObject.put("author_id", blog.get().getAuthor().getId());
-            respObject.put("created_on", blog.get().getCreatedOn().getTime());
+            respObject = mapper.convertValue(blog, ObjectNode.class);
         }
 
         return (new ResponseEntity<ObjectNode>(respObject, HttpStatus.OK));
