c94cad9629b83442f17cc06cdadd4d88e7110746
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-08-10 12:36:31 +0530

    feat: replace place holder with actual logic

--- frontend/src/app/components/blogs/blogs.component.ts
+++ frontend/src/app/components/blogs/blogs.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, Signal, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { BloggingService } from '../../services/blogging/blogging.service';
+import { Blog } from '../../interfaces/blog';
+import { Observable, single } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { BlogRendererComponent } from "../blog-renderer/blog-renderer.component";
 
 @Component({
     selector: 'app-blogs',
-    imports: [RouterLink],
+    imports: [RouterLink, AsyncPipe],
     templateUrl: './blogs.component.html',
     styleUrl: './blogs.component.css'
 })
 export class BlogsComponent {
+    private bloggingService: BloggingService = inject(BloggingService);
+    private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
 
+    blogs$: Observable<Blog[]> = this.bloggingService.getBlogs();
 }
