9b491f9da7bcecf09e1ed1191ad6ed867cb760b4
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-08-10 12:38:04 +0530

    fix: add route for singular blogs

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { BlogRendererComponent } from './components/blog-renderer/blog-renderer.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
     { path: "home", redirectTo: '/' },
     { path: "projects", component: ProjectsComponent},
-    { path: "blogs/", component: BlogsComponent},
+    { path: "blogs", component: BlogsComponent},
+    { path: "blogs/:id", component: BlogRendererComponent},
     { path: "login", component: LoginComponent},
     { path: "register", component: RegisterComponent},
     { path: "profile", component: RegisterComponent}
