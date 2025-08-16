4bf11311f6b89a77f704e00ee8c47126465f5a09
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-08-16 09:45:48 +0530

    feat: create blog editor component

--- frontend/src/app/components/blog-editor/blog-editor.component.ts
+++ frontend/src/app/components/blog-editor/blog-editor.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MarkdownPipe } from 'ngx-markdown';
 
 @Component({
   selector: 'app-blog-editor',
-  imports: [],
+  imports: [MarkdownPipe, AsyncPipe],
   templateUrl: './blog-editor.component.html',
   styleUrl: './blog-editor.component.css'
 })
 export class BlogEditorComponent {
+  private formBuilder = inject(FormBuilder);
 
+  markdownForm!: FormGroup;
+  markdownText: string = "";
+
+  ngOnInit() {
+    this.markdownForm = this.formBuilder.group(
+      {
+        title: ['', [Validators.required]],
+        text: ['', [Validators.required]],
+        tags: ['', [Validators.required]]
+      }
+    )
+  }
+
+  renderMarkdown(event: Event) {
+    this.markdownText = (event.target as HTMLInputElement).value;
+  }
 }
