8a2f6f3443a56a643e1410aa3b49a4b5f799fd87
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-10-20 10:31:54 +0530

    chore: remove blogs component

--- frontend/src/app/components/blogs/blogs.component.ts
+++ frontend/src/app/components/blogs/blogs.component.ts
@@ -1,20 +1,12 @@
-import { Component, inject, Signal, signal } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-import { BloggingService } from '../../services/blogging/blogging.service';
-import { Blog } from '../../interfaces/blog';
-import { Observable, single } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
-import { BlogRendererComponent } from "../blog-renderer/blog-renderer.component";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-blogs',
-    imports: [RouterLink, AsyncPipe],
+    imports: [RouterOutlet],
     templateUrl: './blogs.component.html',
     styleUrl: './blogs.component.css'
 })
 export class BlogsComponent {
-    private bloggingService: BloggingService = inject(BloggingService);
-    private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
 
-    blogs$: Observable<Blog[]> = this.bloggingService.getBlogs();
 }
