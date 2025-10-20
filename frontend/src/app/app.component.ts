8011c38e1187b90aaf6621c365b9ee805752287d
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-10-20 10:33:44 +0530

    chore: remove unused import in app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,22 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { GlslComponent } from "./components/glsl/glsl.component";
-import { ThemeSwitcherComponent } from "./components/theme-switcher/theme-switcher.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { MarkdownModule } from 'ngx-markdown';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GlslComponent } from './components/glsl/glsl.component';
+import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [
-        RouterOutlet,
-        NavbarComponent,
-        GlslComponent,
-        ThemeSwitcherComponent,
-        FooterComponent,
-    ],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    GlslComponent,
+    ThemeSwitcherComponent,
+    FooterComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-}
+export class AppComponent {}
