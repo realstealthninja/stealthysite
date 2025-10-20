788ebe019b97cc677ee38d8112b99048166ebf61
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-10-20 10:31:35 +0530

    chore(backend): add `NonNull` to jwtfilter

--- backend/src/main/java/site/stealthy/backend/Security/JWT/JwtAuthFilter.java
+++ backend/src/main/java/site/stealthy/backend/Security/JWT/JwtAuthFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -35,7 +36,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
      * @throws IOException
      */
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         String authstring = request.getHeader("Authorization");
