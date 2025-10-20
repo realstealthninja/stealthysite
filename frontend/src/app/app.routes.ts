22e703abacda313bcc053445c1b5cb17ec842bfd
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-10-20 10:32:09 +0530

    chore: reroute blogging route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,16 +4,22 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { BlogRendererComponent } from './components/blog-renderer/blog-renderer.component';
-import { BlogEditorComponent } from './components/blog-editor/blog-editor.component';
+import { BlogRoutedRendererComponent } from './components/blogs/blog-routed-renderer/blog-routed-renderer.component';
+import { BlogEditorComponent } from './components/blogs/blog-editor/blog-editor.component';
+import { BlogHomeComponent } from './components/blogs/blog-home/blog-home.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
     { path: "home", redirectTo: '/' },
     { path: "projects", component: ProjectsComponent},
-    { path: "blogs", component: BlogsComponent},
-    { path: "blogs/editor", component: BlogEditorComponent},
-    { path: "blogs/:id", component: BlogRendererComponent},
+    { path: "blogs", component: BlogsComponent,
+      children: [
+
+        {path: '', component: BlogHomeComponent},
+        {path: 'editor', component: BlogEditorComponent},
+        {path: ':id', component: BlogRoutedRendererComponent}
+      ]
+    },
     { path: "login", component: LoginComponent},
     { path: "register", component: RegisterComponent},
     { path: "profile", component: RegisterComponent}
