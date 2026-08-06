e45aa555cc843228fe6242ff75d7844d88cb87e6
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-08-06 18:50:48 +0530

    chore: bump spring to 3.5.16

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.0"
+	id("org.springframework.boot") version "3.5.16"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("co.uzzu.dotenv.gradle") version "4.0.0"
 }
