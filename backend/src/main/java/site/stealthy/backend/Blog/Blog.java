1a2f362213499ab8515f72ca4f1d2919157cd668
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-08-07 05:02:55 +0530

    fix: use LocalDateTime instead of java util date and time

--- backend/src/main/java/site/stealthy/backend/Blog/Blog.java
+++ backend/src/main/java/site/stealthy/backend/Blog/Blog.java
@@ -1,6 +1,6 @@
 package site.stealthy.backend.Blog;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
@@ -15,8 +15,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import site.stealthy.backend.User.User;
 
 @Entity
@@ -34,10 +32,8 @@ public class Blog {
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdOn;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date editedOn;
+    private LocalDateTime createdOn;
+    private LocalDateTime editedOn;
 
     @ManyToOne
     @JoinColumn(name = "author_id", nullable = false)
@@ -46,112 +42,112 @@ public class Blog {
     @OneToMany
     private Set<Comment> comments;
 
-    /** 
+    /**
      * @return long
      */
     public long getId() {
         return id;
     }
 
-    /** 
+    /**
      * @param id
      */
     public void setId(long id) {
         this.id = id;
     }
 
-    /** 
+    /**
      * @return String
      */
     public String getTitle() {
         return title;
     }
 
-    /** 
+    /**
      * @param title
      */
     public void setTitle(String title) {
         this.title = title;
     }
 
-    /** 
+    /**
      * @return String
      */
     public String getContent() {
         return content;
     }
 
-    /** 
+    /**
      * @param content
      */
     public void setContent(String content) {
         this.content = content;
     }
 
-    /** 
-     * @return Date
-     */
-    public Date getCreatedOn() {
-        return createdOn;
-    }
-
-    /** 
-     * @param createdOn
-     */
-    public void setCreatedOn(Date createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    /** 
-     * @return Date
-     */
-    public Date getEditedOn() {
-        return editedOn;
-    }
-
-    /** 
-     * @param editedOn
-     */
-    public void setEditedOn(Date editedOn) {
-        this.editedOn = editedOn;
-    }
-
-    /** 
+    /**
      * @return User
      */
     public User getAuthor() {
         return author;
     }
 
-    /** 
+    /**
      * @param author
      */
     public void setAuthor(User author) {
         this.author = author;
     }
 
-    /** 
+    /**
+     * @return LocalDateTime
+     */
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    /***
+     * @param createdOn
+     */
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    /**
+     * @return LocalDateTime
+     */
+    public LocalDateTime getEditedOn() {
+        return editedOn;
+    }
+
+    /**
+     * @param editedOn
+     */
+    public void setEditedOn(LocalDateTime editedOn) {
+        this.editedOn = editedOn;
+    }
+
+    /**
      * @return Set<Comment>
      */
     public Set<Comment> getComments() {
         return comments;
     }
 
-    /** 
+    /**
      * @param comments
      */
     public void setComments(Set<Comment> comments) {
         this.comments = comments;
     }
 
-    /** 
+    /**
      * @return Set<String>
      */
     public Set<String> getTags() {
         return tags;
     }
 
-    /** 
+    /**
      * @param tags
      */
     public void setTags(Set<String> tags) {
