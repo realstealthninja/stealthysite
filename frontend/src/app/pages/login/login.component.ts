e8c099b138b8bb1c978a968e8b0f0861e7231e47
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-08-06 18:05:24 +0530

    lint: login

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit, inject } from '@angular/core';
-import {FormGroup, FormControl, ReactiveFormsModule, Validators, FormBuilder} from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { LoginDTO } from '../../interfaces/LoginDTO';
 import { UserauthService } from '../../services/userauth/userauth.service';
 
-
 @Component({
-    selector: 'app-login',
-    imports: [ReactiveFormsModule, RouterLink],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.css'
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
 })
 export class LoginComponent implements OnInit {
   private formBuilder = inject(FormBuilder);
@@ -18,12 +23,19 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
 
   ngOnInit() {
-
     this.loginForm = this.formBuilder.group({
-      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/)]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(
+            /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/,
+          ),
+        ],
+      ],
       username: ['', [Validators.required]],
-    })
-
+    });
   }
 
   get password() {
@@ -31,15 +43,14 @@ export class LoginComponent implements OnInit {
   }
 
   get username() {
-    return this.loginForm.get('username')
+    return this.loginForm.get('username');
   }
   onSubmit(_: FormData) {
-
     const user: LoginDTO = {
       username: this.username?.value,
-      password: this.password?.value
-    }
-    
+      password: this.password?.value,
+    };
+
     this.userAuth.loginUser(user);
   }
 }
