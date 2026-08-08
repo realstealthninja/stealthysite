019872607c1ae74535948093f1ef46f600e70169
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-08-08 21:10:38 +0530

    fix: use String in JPArepository

--- backend/src/main/java/site/stealthy/backend/User/UserRepository.java
+++ backend/src/main/java/site/stealthy/backend/User/UserRepository.java
@@ -6,12 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByusername(String username);
 
     Optional<User> findByEmail(String email);
-    
+
     Optional<User> findByUserid(String userid);
 
     boolean existsByusername(String Username);
