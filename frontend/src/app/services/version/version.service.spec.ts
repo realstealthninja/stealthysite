2b9f4273661abffc4eff02d2774b6dc4b0ba494c
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-08-11 07:45:15 +0530

    test: fix tests for versions

--- frontend/src/app/services/version/version.service.spec.ts
+++ frontend/src/app/services/version/version.service.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
-import { VersionService } from './version.service';
+import { HttpResourceRef, provideHttpClient } from '@angular/common/http';
+
 import {
   HttpTestingController,
   provideHttpClientTesting,
 } from '@angular/common/http/testing';
+
+import { ApplicationRef } from '@angular/core';
+import { VersionResp } from '../../interfaces/version';
+import { VersionService } from './version.service';
 
 describe('VersionService', () => {
   let service: VersionService;
@@ -12,7 +17,7 @@ describe('VersionService', () => {
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClientTesting()],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     });
 
     httpTesting = TestBed.inject(HttpTestingController);
@@ -24,29 +29,41 @@ describe('VersionService', () => {
   });
 
   it('should get java version', async () => {
-    TestBed.runInInjectionContext(async () => {
-      const body = { name: 'java', version: '21.0.11+10-LTS' };
-      const javaVersion = service.getJavaVersion();
-      const req = httpTesting.expectOne('/api/v1/version/java');
+    const body = { name: 'java', version: '21.0.11+10-LTS' };
+    let javaVersion!: HttpResourceRef<VersionResp | undefined>;
 
-      expect(req.request.method).toBe('GET');
-
-      req.flush(body);
-      expect(await javaVersion.hasValue()).toEqual(true);
-      expect(await javaVersion.value()).toEqual(body);
+    TestBed.runInInjectionContext(() => {
+      javaVersion = service.getJavaVersion();
     });
+    TestBed.tick(); // ensure that the request is sent
+
+    const req = httpTesting.expectOne('/api/v1/version/java');
+    expect(req.request.method).toBe('GET');
+    req.flush(body);
+
+    await TestBed.inject(ApplicationRef).whenStable(); // complete the async operation before checking
+
+    expect(javaVersion.hasValue()).toEqual(true);
+    expect(javaVersion.value()).toEqual(body);
   });
 
-  it('should get spring version', () => {
-    TestBed.runInInjectionContext(async () => {
-      const body = { name: 'spring', version: '7.0.8' };
-      const springVersion = service.getSpringVersion();
+  it('should get spring version', async () => {
+    const body = { name: 'spring', version: '7.0.8' };
+    let springVersion!: HttpResourceRef<VersionResp | undefined>;
 
-      const req = httpTesting.expectOne('/api/v1/version/spring');
-      expect(req.request.method).toBe('GET');
-      req.flush(body);
-      expect(await springVersion.hasValue()).toEqual(true);
-      expect(await springVersion.value()).toEqual(body);
+    TestBed.runInInjectionContext(() => {
+      springVersion = service.getSpringVersion();
     });
+
+    TestBed.tick();
+
+    const req = httpTesting.expectOne('/api/v1/version/spring');
+    expect(req.request.method).toBe('GET');
+    req.flush(body);
+
+    await TestBed.inject(ApplicationRef).whenStable();
+
+    expect(springVersion.hasValue()).toEqual(true);
+    expect(springVersion.value()).toEqual(body);
   });
 });
