7af8e0a3b11f43a20661a3de1743626286d46aa9
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-08-11 08:40:31 +0530

    chore: remove unused imports

--- backend/src/main/java/site/stealthy/backend/Security/WebSecurityConfig.java
+++ backend/src/main/java/site/stealthy/backend/Security/WebSecurityConfig.java
@@ -14,8 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-
 import site.stealthy.backend.Security.JWT.JwtAuthFilter;
 import site.stealthy.backend.User.UserService;
 
